e69bd1cfb85394c115864e4c4c6c14f2bac7b64c
Jun-cloud <kjy8827@gmail.com>
2021-09-15 20:38:05 +0900

    Add Scheduler

--- crawling.py
+++ crawling.py
@@ -2,28 +2,32 @@ import requests
 from bs4 import BeautifulSoup
 import telegram
 from token import api_key
+from apscheduler.schedulers.blocking import BlockingScheduler
 
 bot = telegram.Bot(token=api_key)
 # url = 'http://www.cgv.co.kr/common/showtimes/iframeTheater.aspx?areacode=02&theatercode=0012&date=20210729'
 url = 'http://www.cgv.co.kr/common/showtimes/iframeTheater.aspx?areacode=02&theatercode=0012&date=20210806'
-html = requests.get(url)
-soup = BeautifulSoup(html.text, 'html.parser')
 
-# 특정 영화 제목만 가져와보기
-# print(soup.select_one('body > div > div.sect-showtimes > ul > li:nth-child(1) > div > div.info-movie > a > strong'))
+def scrap_movie():
+    html = requests.get(url)
+    soup = BeautifulSoup(html.text, 'html.parser')
 
-# 모든 영화 제목 가져오기
-# title_list = soup.select('div.info-movie')
-# for i in title_list:
-#     print(i.select_one('a > strong').text.strip())
+    # 특정 영화 제목만 가져와보기
+    # print(soup.select_one('body > div > div.sect-showtimes > ul > li:nth-child(1) > div > div.info-movie > a > strong'))
 
-imax = soup.select('span.imax')
-for i in imax:
-    imax_title = i.find_parent('div', class_='col-times').select_one('div.info-movie > a > strong').text.strip()
-    print(imax_title)
+    # 모든 영화 제목 가져오기
+    # title_list = soup.select('div.info-movie')
+    # for i in title_list:
+    #     print(i.select_one('a > strong').text.strip())
 
-if(imax):
-    bot.send_message(chat_id=1955432261, text='IMAX 예매가 열렸습니다.')
-else:
-    bot.send_message(chat_id=1955432261, text='IMAX 예매가 아직 열리지 않았습니다.')
+    imax = soup.select('span.imax')
+    if(imax):
+        for i in imax:
+            imax_title = i.find_parent('div', class_='col-times').select_one('div.info-movie > a > strong').text.strip()
+            bot.send_message(chat_id=1955432261, text=imax_title + ' IMAX 예매가 열렸습니다.')
+    else:
+        bot.send_message(chat_id=1955432261, text='IMAX 예매가 아직 열리지 않았습니다.')
 
+
+sched = BlockingScheduler()
+sched.add_job(scrap_movie(), 'interval', seconds=30)
